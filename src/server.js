af9a10395b5af9a7e6b92a3daf1581af935c09e9
Abu A'la <abualaanadwi1@gmail.com>
2023-05-06 15:41:59 +0700

    Hapi-Cors added

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 const Hapi = require('@hapi/hapi'); // for Hapi
 const routes = require('./routes');
+const HapiCors = require('hapi-cors');
 
-const init = async () => { // 
+const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production'?'localhost':'0.0.0.0',
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
+  });
+
+  // Register the CORS middleware plugin
+  await server.register({
+    plugin: HapiCors,
+    options: {
+      origins: ['*'], // Replace with your own domain
     },
   });
 
@@ -18,3 +22,4 @@ const init = async () => { //
 }
 
 init()
+
